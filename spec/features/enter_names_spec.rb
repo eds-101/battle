4861befc69f7c3c26febe4fce7c08fd1be16b772
Eds <steadyede@live.co.uk>
2021-02-17 16:13:50 +0000

    Refactored tests

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'James'
-    fill_in :player_2_name, with: 'Yusif'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'James vs. Yusif'
   end
 end
--- spec/features/viewing_hit_points_spec.rb
+++ spec/features/viewing_hit_points_spec.rb
@@ -1,17 +1,11 @@
 feature 'viewing hit points' do
   scenario 'shows player 1 hit points' do
-    visit '/'
-    fill_in :player_1_name, with: 'Frank'
-    fill_in :player_2_name, with: 'Ed'
-    click_button 'Submit'
-    expect(page).to have_content 'Frank: 100 HP'
+    sign_in_and_play
+    expect(page).to have_content 'James: 100 HP'
   end
 
-  scenario 'shows player 1 hit points' do
-    visit '/'
-    fill_in :player_1_name, with: 'Frank'
-    fill_in :player_2_name, with: 'Ed'
-    click_button 'Submit'
-    expect(page).to have_content 'Ed: 100 HP'
+  scenario 'shows player 2 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Yusif: 100 HP'
   end
 end
