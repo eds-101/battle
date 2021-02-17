175df0d52e54020265f825af42c182c7580410b8
Eds <steadyede@live.co.uk>
2021-02-17 15:11:49 +0000

    Screen to fill in names

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,9 @@
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'James'
+    fill_in :player_2_name, with: 'Yusif'
+    click_button 'Submit'
+    expect(page).to have_content 'James vs. Yusif'
+  end
+end
